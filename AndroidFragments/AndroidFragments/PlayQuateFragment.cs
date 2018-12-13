82a3ec37af7eba4dc0c84995fb14b670de9efa85
Estimon <ploompuumartin@gmail.com>
2018-12-13 09:41:27 +0200

    fragment example

---
+++ AndroidFragments/AndroidFragments/PlayQuateFragment.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace AndroidFragments
+{
+    public class PlayQuateFragment : Fragment
+    {
+        public int PlayId => Arguments.GetInt("current_play_id", 0);
+
+        public static PlayQuateFragment newInstacne(int PlayId)
+        {
+            var bundel = new Bundle();
+            bundel.PutInt("current_play_id", PlayId);
+            return new PlayQuateFragment { Arguments = bundel };
+        }
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            if(container == null)
+            {
+                return null;
+            }
+
+            var textview = new TextView(Activity);
+            var padding = Convert.ToInt32(TypedValue.ApplyDimension(ComplexUnitType.Dip, 4, Activity.Resources.DisplayMetrics));
+            textview.SetPadding(padding, padding, padding, padding);
+            textview.TextSize = 24;
+            textview.Text = shakespear.Dialogue[PlayId];
+
+            var scroller = new ScrollView(Activity);
+            scroller.AddView(textview);
+            return scroller;
+
+
+        }
+    }
+}
---
+++ AndroidFragments/AndroidFragments/TitlesFragment.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace AndroidFragments
+{
+    public class TitlesFragment : ListFragment
+    {
+        int selectedPlayId;
+        bool showingTwoFragments;
+
+        public TitlesFragment()
+        {
+
+        }
+
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            base.OnActivityCreated(savedInstanceState);
+
+
+            var quoteContainer = Activity.FindViewById(Resource.Id.playquote_container);
+            showingTwoFragments = quoteContainer != null &&
+                                  quoteContainer.Visibility == ViewStates.Visible;
+            if (showingTwoFragments)
+            {
+                ListView.ChoiceMode = ChoiceMode.Single;
+                ShowPlayQuote(selectedPlayId);
+            }
+        }
+
+        public override void OnSaveInstanceState(Bundle outState)
+        {
+            base.OnSaveInstanceState(outState);
+            outState.PutInt("current_play_id", selectedPlayId);
+        }
+
+        public override void OnListItemClick(ListView l, View v, int position, long id)
+        {
+            ShowPlayQuote(position);
+        }
+
+        void ShowPlayQuote(int playId)
+        {
+            selectedPlayId = playId;
+            if (showingTwoFragments)
+            {
+                ListView.SetItemChecked(selectedPlayId, true);
+
+                var playQuoteFragment = FragmentManager.FindFragmentById(Resource.Id.playquote_container) as PlayQuateFragment;
+
+                if (playQuoteFragment == null || playQuoteFragment.PlayId != playId)
+                {
+                    var container = Activity.FindViewById(Resource.Id.playquote_container);
+                    var quoteFrag = PlayQuateFragment.newInstacne(selectedPlayId);
+
+                    FragmentTransaction ft = FragmentManager.BeginTransaction();
+                    ft.Replace(Resource.Id.playquote_container, quoteFrag);
+                    ft.Commit();
+                }
+            }
+            else
+            {
+                var intent = new Intent(Activity, typeof(PlayQuateAct));
+                intent.PutExtra("current_play_id", playId);
+                StartActivity(intent);
+            }
+        }
+
+
+
+
+    }
+}
